48009c718b1a67ceb3cf074ced9a835ea3b0884e
MethodFunc <tjfgusfkd@gmail.com>
2020-06-10 16:10:26 +0900

    2020

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KSA_edu_Daejeon_Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
